83f0518e475e52ede534d8f677f38d205724898a
Yonas <yonas@My-MacBook-Air.local>
2019-08-02 19:10:08 +0400

    updates

--- app/src/main/java/com/project/weatherapp/WeatherAdapter.java
+++ app/src/main/java/com/project/weatherapp/WeatherAdapter.java
@@ -3,6 +3,7 @@ package com.project.weatherapp;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,6 +44,7 @@ public class WeatherAdapter extends ArrayAdapter<Weather> {
             TextView min_temp = listItem.findViewById(R.id.min_temp);
             ImageView avatar = listItem.findViewById(R.id.icon);
             ImageView menu = listItem.findViewById(R.id.menu);
+            View layout = listItem.findViewById(R.id.constraintLayout);
 
             weather_pre.setText(current_weather.getWeather());
             location.setText(current_weather.getLocation());
@@ -56,36 +58,47 @@ public class WeatherAdapter extends ArrayAdapter<Weather> {
             System.out.println(weather);
             if(weather.contains("sunny")){
                 avatar.setImageResource(R.drawable.weather_sunny);
+                layout.setBackground(getContext().getDrawable(R.drawable.bg_color_hot));
             }
             if(weather.contains("partly cloud")){
                 avatar.setImageResource(R.drawable.weather_partly_cloudy);
+                layout.setBackground(getContext().getDrawable(R.drawable.bg_color_cloudy));
             }
             if(weather.contains("sunny intervals")){
                 avatar.setImageResource(R.drawable.weather_partly_cloudy);
+                layout.setBackground(getContext().getDrawable(R.drawable.bg_color_clear));
             }
             if(weather.contains("light rain")){
                 avatar.setImageResource(R.drawable.weather_rainy);
+                layout.setBackground(getContext().getDrawable(R.drawable.bg_color_storm));
             }
             if(weather.contains("heavy rain")){
                 avatar.setImageResource(R.drawable.weather_pouring);
+                layout.setBackground(getContext().getDrawable(R.drawable.bg_color_storm));
             }
             if(weather.contains("sleet shower")){
                 avatar.setImageResource(R.drawable.weather_snowy_rainy);
+                layout.setBackground(getContext().getDrawable(R.drawable.bg_color_sleet));
             }
             if(weather.contains("hail shower")){
                 avatar.setImageResource(R.drawable.weather_hail);
+                layout.setBackground(getContext().getDrawable(R.drawable.bg_color_clear_night));
             }
             if(weather.contains("heavy snow")){
                 avatar.setImageResource(R.drawable.weather_snowy_rainy);
+                layout.setBackground(getContext().getDrawable(R.drawable.bg_color_sleet));
             }
             if(weather.contains("thunder")){
                 avatar.setImageResource(R.drawable.weather_lightning);
+                layout.setBackground(getContext().getDrawable(R.drawable.bg_color_thunder));
             }
             if(weather.contains("clear sky")){
                 avatar.setImageResource(R.drawable.weather_sunny);
+                layout.setBackground(getContext().getDrawable(R.drawable.bg_color_clear));
             }
             if(weather.contains("light cloud")){
                 avatar.setImageResource(R.drawable.weather_cloudy);
+                layout.setBackground(getContext().getDrawable(R.drawable.bg_color_cloudy));
             }
 
             if(position == 0){
